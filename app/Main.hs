51b7c37373452c5e6c9fb05d7474f142908c50b0
TOMOYUKI KAWANO <kawatomo@city.co.jp>
2018-09-18 13:01:37 +0900

    SQLのパラメータ値の埋め込みと整形を実装.

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,15 @@
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
-import Lib
-import System.Exit
+import           Control.Lens
+import           Data.Aeson
+import qualified Data.ByteString.Char8 as B
+import           GHC.Generics
+import           Lib
+import           Network.HTTP.Simple
+import           System.Exit
 
 main :: IO ()
 main = do
@@ -10,4 +18,15 @@ main = do
     Left err -> do
       putStrLn $ show err
       exitWith $ ExitFailure 1
-    Right s  -> putStrLn s
+    Right s  -> pretty s >>= putStrLn
+
+data Res = Res{ result :: String }
+  deriving (Generic, Show)
+instance FromJSON Res
+
+pretty :: Sql -> IO Sql
+pretty sql = do
+  let req = setRequestBodyURLEncoded [("reindent", "1"), ("sql", B.pack sql)]
+          $ setRequestMethod "POST" "https://sqlformat.org/api/v1/format"
+  res::Res <- httpJSON req >>= return . getResponseBody
+  return $ result res
